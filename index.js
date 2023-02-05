44c436785c4dec19d5464e0e5d64afe923e6f6d0
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-05 17:33:15 -0600

    update package

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ module.exports = async function makeIndexFetch (opts = {}) {
       if(mainURL.hostname === '_'){
         const detectedPort = await detect(mainConfig.port)
         const isItRunning = mainConfig.port !== detectedPort
-        return {statusCode: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, data: [String(isItRunning)]}
+        return {status: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, body: [String(isItRunning)]}
       }
 
       request.url = request.url.replace('oui', 'http')
@@ -51,7 +51,7 @@ module.exports = async function makeIndexFetch (opts = {}) {
       }
 
       const res = await got(request)
-      return sendTheData(signal, {statusCode: res.statusCode, headers: res.headers, data: [res.body]})
+      return sendTheData(signal, {status: res.statusCode, headers: res.headers, body: [res.body]})
   }
 
   async function handleOuis(request) {
@@ -67,7 +67,7 @@ module.exports = async function makeIndexFetch (opts = {}) {
       if(mainURL.hostname === '_'){
         const detectedPort = await detect(mainConfig.port)
         const isItRunning = mainConfig.port !== detectedPort
-        return {statusCode: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, data: [String(isItRunning)]}
+        return {status: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, body: [String(isItRunning)]}
       }
 
       request.url = request.url.replace('oui', 'http')
@@ -84,7 +84,7 @@ module.exports = async function makeIndexFetch (opts = {}) {
       }
 
     const res = await got(request)
-    return sendTheData(signal, {statusCode: res.statusCode, headers: res.headers, data: [res.body]})
+    return sendTheData(signal, {status: res.statusCode, headers: res.headers, body: [res.body]})
   }
   router.any('oui://*/**', handleOui)
   router.any('ouis://*/**', handleOuis)
